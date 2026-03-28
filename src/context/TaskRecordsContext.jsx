1d5cf71039caf2ce6901ca22a3798938b2589649
pope04527-a11y <pope04527@gmail.com>
2026-03-28 09:48:02 +0300

    Update TaskRecordsContext.jsx

--- src/context/TaskRecordsContext.jsx
+++ src/context/TaskRecordsContext.jsx
@@ -4,7 +4,7 @@ const TaskRecordsContext = createContext();
 
 export const TaskRecordsProvider = ({ children }) => {
   const [records, setRecords] = useState([]);
-  const BASE_URL = "https://stacksapp-backend.onrender.com";
+  const BASE_URL = "https://stacks2-backend.onrender.com";
 
   // Fetch records from backend
   const fetchTaskRecords = async () => {
